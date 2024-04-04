feaa68313119f78aec3cd28096211e52d8309bd5
Baran <boybeybaran5@gmail.com>
2024-04-03 23:47:30 -0400

    TicketingProjectApplication.java, and html files are created

--- src/test/java/com/company/TicketingProjectApplicationTests.java
+++ src/test/java/com/company/TicketingProjectApplicationTests.java
@@ -2,12 +2,10 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
-class TicketingProjectMvcApplicationTests {
+public class TicketingProjectApplicationTests {
+    @Test
+    void contextLoads() {
 
-	@Test
-	void contextLoads() {
-	}
-
+    }
 }
