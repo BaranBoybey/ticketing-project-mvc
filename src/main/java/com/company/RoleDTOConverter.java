f25024a1ce84b15e32ef52bd94bac9ddc26a7990
Baran <boybeybaran5@gmail.com>
2024-04-06 03:01:15 -0400

    User create function completed

---
+++ src/main/java/com/company/RoleDTOConverter.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import com.company.dto.RoleDTO;
+import com.company.service.RoleService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class RoleDTOConverter implements Converter<String, RoleDTO> {
+
+    private final RoleService roleService;
+
+    public RoleDTOConverter(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public RoleDTO convert(String source) {
+        return roleService.findById(Long.parseLong(source));
+    }
+}
--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -1,16 +1,52 @@
 package com.company.controller;
 
+import com.company.dto.UserDTO;
+import com.company.service.RoleService;
+import com.company.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
-    public String createUser() {
+    public String createUser(Model model) {
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roleList", roleService.findAll());
+        model.addAttribute("userList", userService.findAll());
         return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(Model model, @ModelAttribute("user") UserDTO userDTO) {
+        userService.save(userDTO);
+
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roleList", roleService.findAll());
+        model.addAttribute("userList", userService.findAll());
+
+        return "/user/create";
+
+
+
+
+
+
     }
 
 
