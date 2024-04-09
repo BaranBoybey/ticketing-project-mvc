409bb676360817ed088a0a057677d0514145b379
Baran <boybeybaran5@gmail.com>
2024-04-08 22:48:06 -0400

    Controller, dto, and entity classes created for Project object.

---
+++ src/main/java/com/company/dto/ProjectDTO.java
@@ -0,0 +1,12 @@
+package com.company.dto;
+
+import java.time.LocalDate;
+
+public class ProjectDTO {
+    private String projectName;
+    private String projectCode;
+    private UserDTO assignedManager;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+}
---
+++ src/main/java/com/company/entity/Project.java
@@ -0,0 +1,13 @@
+package com.company.entity;
+
+import com.company.dto.UserDTO;
+
+import java.time.LocalDate;
+
+public class Project extends BaseEntity{
+    private String projectName;
+    private String projectCode;
+    private UserDTO assignedManager;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
