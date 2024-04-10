b38ea7b262923c4eb5419d61a85d3a5b82c3af8d
Baran <boybeybaran5@gmail.com>
2024-04-09 22:59:27 -0400

    findManagers service created for project create page

--- src/main/java/com/company/service/impl/UserServiceImpl.java
+++ src/main/java/com/company/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.company.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
@@ -35,4 +36,8 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
         super.update(object.getUserName(), object);
     }
 
+    @Override
+    public List<UserDTO> findManagers() {
+        return super.findAll().stream().filter(userDTO -> userDTO.getRole().getId() == 2).collect(Collectors.toList());
+    }
 }
